daacaa0272084970eb7f4af336a88b613e5d7ef9
Maksim Yermalovich <yermalovich.m@gmail.com>
2016-07-04 18:45:49 +0300

    add configure btn to panel widget

--- src/components/widgets/panel/widget.js
+++ src/components/widgets/panel/widget.js
@@ -2,19 +2,29 @@ var React = require('react'),
 
     OS = require('os'),
     Widget = OS.Widget,
+    Configurator = OS.Configurator,
+    Link = OS.Link,
 
     settings = require('./widget_settings');
 
 var _Widget = React.createClass({
   getInitialState: function () {
     return {
-      widgetStyles: settings.DEFAULT_WIDGET_STYLES
+      widgetStyles: settings.DEFAULT_WIDGET_STYLES,
+      shortcutStyles: settings.DEFAULT_SHORTCUT_STYLES,
+      hoverShortcutStyles: settings.DEFAULT_HOVER_SHORTCUT_STYLES
     };
   },
 
   render: function () {
     return (
       <Widget.Widget widgetStyles={ this.state.widgetStyles }>
+        <Link
+          style={ this.state.shortcutStyles }
+          hoverStyle={ this.state.hoverShortcutStyles }>
+
+          <span className="glyphicon glyphicon-cog" />
+        </Link>
       </Widget.Widget>
     );
   }
--- src/components/widgets/panel/widget_settings.js
+++ src/components/widgets/panel/widget_settings.js
@@ -3,15 +3,35 @@ var _ = require('underscore'),
     OS = require('os'),
     globalSettings = OS.settings;
 
+var DEFAULT_SHORTCUT_STYLES = {
+  width: '100%',
+  display: 'inline-block',
+  textAlign: 'center',
+  lineHeight: '48px',
+  fontSize: '48px',
+  color: '#333',
+  marginTop: '10px'
+};
+
 var settings = {
   DEFAULT_WIDGET_STYLES: _.extend(
     globalSettings.DEFAULT_WIDGET_STYLES,
     {
       width: '60px',
       height: '100%',
-      top: '0',
-      left: '0',
-      borderRadius: '0'
+      top: 0,
+      left: 0,
+      borderRadius: 0
+    }
+  ),
+
+  DEFAULT_SHORTCUT_STYLES: DEFAULT_SHORTCUT_STYLES,
+  DEFAULT_HOVER_SHORTCUT_STYLES: _.extend(
+    _.clone(DEFAULT_SHORTCUT_STYLES),
+    {
+      lineHeight: '52px',
+      fontSize: '52px',
+      color: '#111'
     }
   )
 };
