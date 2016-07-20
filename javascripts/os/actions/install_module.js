09aebc17e1e81415ecbf6cb3d6924846682ccec0
Maksim Yermalovich <yermalovich.m@gmail.com>
2016-07-20 12:35:22 +0300

    refactoring installModule

--- javascripts/os/actions/install_module.js
+++ javascripts/os/actions/install_module.js
@@ -1,11 +1,31 @@
 var sprintf = require('sprintf-js').sprintf,
 
     Modules = require('../modules'),
+    isUrl = require('./is_url'),
     log = require('./log');
 
-var installModule = function (name, module) {
-  log('info', sprintf('install module %s.', name));
+var installModule = function () {
+  var url = arguments[0],
+      name = arguments[0],
+      module = arguments[1];
 
+  if (arguments.length === 2 && !_.isEmpty(name) && _.isObject(module)) {
+    return _installModule(name, module);
+  }
+
+  if (arguments.length === 1 && !_.isEmpty(url) && isUrl(url)) {
+    return _installModuleByUrl(url);
+  }
+
+  log('error', 'installModules - invalid arguments.');
+};
+
+var _installModuleByUrl = function (name, url) {
+  console.log(name, url);
+};
+
+var _installModule = function (name, module) {
+  log('info', sprintf('install module %s.', name));
   Modules.add(name, module);
 };
 
