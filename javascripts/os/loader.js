97754d185ee2bfee798d31537a507b8d22363a83
Maksim Yermalovich <yermalovich.m@gmail.com>
2016-07-24 17:05:07 +0300

    refactoring Scripts - AppDispatcher & Events

--- javascripts/os/loader.js
+++ javascripts/os/loader.js
@@ -1,10 +1,13 @@
 var Logger = require('./logger'),
-    Modules = require('./modules');
+    Modules = require('./modules'),
+    Scripts = require('./scripts');
 
 var Loader = function () {
   this.load = function (callback) {
     new Logger();
+
     global.Modules = new Modules(function () {
+      global.Scripts = new Scripts();
       callback();
     });
   };
--- javascripts/os/scripts.js
+++ javascripts/os/scripts.js
@@ -32,20 +32,6 @@ var Scripts = function () {
   this.list = [];
   var _this = this;
 
-  log('info', 'Scripts - start loading.');
-  storage.get(settings.SCRIPTS_STORAGE_KEY, function (scripts) {
-    _this.list = scripts || [];
-
-    _.each(_this.list, function (script) {
-      addScript(script);
-    });
-    AppDispatcher.updatedScripts(_this.list);
-
-    AppDispatcher.bind(Events.installScript, function (script) {
-      _this.add(script);
-    });
-  });
-  log('info', 'Scripts - finish loading.');
 
   this.all = function () {
     return _this.list;
@@ -72,26 +58,24 @@ var Scripts = function () {
     AppDispatcher.bind(Events.updatedScripts, callback);
   };
 
+  storage.get(settings.SCRIPTS_STORAGE_KEY, function (scripts) {
+    _this.list = scripts || [];
+
+    _.each(_this.list, function (script) {
+      addScript(script);
+    });
+    AppDispatcher.updatedScripts(_this.list);
+
+    AppDispatcher.bind(Events.installScript, function (script) {
+      _this.add(script);
+    });
+
+    AppDispatcher.bind(Events.uninstallScript, function (script) {
+      _this.remove(script);
+    });
+  });
+
   log('info', 'Finish initializing Scripts.');
 };
 
-var Singleton = (function () {
-  var instance;
-
-  function createInstance () {
-    var instance = new Scripts();
-    return instance;
-  }
-
-  return {
-    getInstance: function () {
-      if (!instance) {
-        instance = createInstance();
-      }
-
-      return instance;
-    }
-  };
-}) ();
-
-module.exports = Singleton.getInstance();
+module.exports = Scripts;
